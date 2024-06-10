da64e5d36890ce98895d0c83a1a09b4e174854e2
Shonki56 <joegittings180@gmail.com>
2024-06-10 22:59:33 +0100

    Completed 05_sumAll Cleaned it up a lot

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,20 +1,17 @@
 const sumAll = function (from, until) {
   let sum = 0;
+
   if (typeof from != "number" || typeof until != "number") {
     return "ERROR";
   } else if (from <= 0 || until <= 0) {
     return "ERROR";
   } else if (from > until) {
-    for (until; until < from; until++) {
-      sum += until;
-    }
-    return sum + from;
+    sum = (from * (until + from)) / 2;
   } else {
-    for (from; from < until; from++) {
-      sum += from;
-    }
-    return sum + until;
+    sum = (until * (from + until)) / 2;
   }
+
+  return sum;
 };
 
 // Do not edit below this line

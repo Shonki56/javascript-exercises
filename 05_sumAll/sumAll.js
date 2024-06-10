aff13217d9a64c1e1bc3beaf3f74ed271b13e269
Shonki56 <joegittings180@gmail.com>
2024-06-10 19:53:46 +0100

    Done but crappy way MAKE BETTER

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
-
+const sumAll = function (from, until) {
+  let sum = 0;
+  if (typeof from != "number" || typeof until != "number") {
+    return "ERROR";
+  } else if (from <= 0 || until <= 0) {
+    return "ERROR";
+  } else if (from > until) {
+    for (until; until < from; until++) {
+      sum += until;
+    }
+    return sum + from;
+  } else {
+    for (from; from < until; from++) {
+      sum += from;
+    }
+    return sum + until;
+  }
 };
 
 // Do not edit below this line

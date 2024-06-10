3aee6e4403852a0aca759bdd151960d6d73438d4
Shonki56 <joegittings180@gmail.com>
2024-06-10 19:39:52 +0100

    Completed 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function (numbers, ...numsToBeRemoved) {
+  for (const arg of numsToBeRemoved) {
+    for (let j = 0; j < numbers.length; j++) {
+      if (numbers[j] === arg) {
+        let index = numbers.indexOf(numbers[j]);
+        numbers.splice(index, 1);
+        j--;
+      }
+    }
+  }
+
+  return numbers;
 };
 
 // Do not edit below this line

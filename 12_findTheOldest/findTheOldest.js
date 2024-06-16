6fb528dfa57cc752f2cbddcfa9497468ebf089ec
Shonki56 <joegittings180@gmail.com>
2024-06-16 14:28:15 +0100

    Able to do first test

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,31 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (arr) {
+    let ages = arr.map((person) => ({
+        ...person,
+        age: person.yearOfDeath - person.yearOfBirth,
+    }));
+    console.table(ages);
+    ages.sort((a, b) => a.age - b.age).reverse();
+    console.log(ages[0]);
+    return ages[0];
 };
 
+const people = [
+    {
+        name: "Carly",
+        yearOfBirth: 1942,
+        yearOfDeath: 1970,
+    },
+    {
+        name: "Ray",
+        yearOfBirth: 1962,
+        yearOfDeath: 2011,
+    },
+    {
+        name: "Jane",
+        yearOfBirth: 1912,
+        yearOfDeath: 1941,
+    },
+];
+findTheOldest(people);
 // Do not edit below this line
 module.exports = findTheOldest;

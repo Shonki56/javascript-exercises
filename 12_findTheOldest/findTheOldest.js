f4475a7b5f0a79a666ad4f2c3e3a7b0cd10ed3c5
Shonki56 <joegittings180@gmail.com>
2024-06-16 15:43:48 +0100

    Completed findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,19 +1,21 @@
 const findTheOldest = function (arr) {
-    let ages = arr.map((person) => ({
-        ...person,
-        age: person.yearOfDeath - person.yearOfBirth,
-    }));
-    console.table(ages);
-    ages.sort((a, b) => a.age - b.age).reverse();
-    console.log(ages[0]);
-    return ages[0];
+    let newAges = arr.map((person) => {
+        let currentYear = new Date().getFullYear();
+        if (person.hasOwnProperty("yearOfDeath")) {
+            return { ...person, age: person.yearOfDeath - person.yearOfBirth };
+        } else {
+            return { ...person, age: currentYear - person.yearOfBirth };
+        }
+    });
+    console.table(newAges);
+    //newAges.sort((a, b) => a.age - b.age).reverse();
+    return newAges.sort((a, b) => b.age - a.age)[0];
 };
 
 const people = [
     {
         name: "Carly",
         yearOfBirth: 1942,
-        yearOfDeath: 1970,
     },
     {
         name: "Ray",

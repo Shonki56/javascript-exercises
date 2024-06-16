3037c978ae67d1484d12f17ddbc5a9356f4375d5
Shonki56 <joegittings180@gmail.com>
2024-06-16 14:28:34 +0100

    Done palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    let arr = string.split(" ");
+    if (arr.includes(".")) {
+        arr.pop();
+    }
+    arr[0].toLowerCase;
+    return arr.reverse().join(" ") == string;
 };
 
 // Do not edit below this line
